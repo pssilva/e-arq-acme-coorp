ea5d520c0d0efd665db7124bc9e026fe5cbfd389
Paulo Sérgio da Silva <pss1suporte@gmail.com>
2017-08-24 16:40:20 -0300

    Estatistica Admin!

--- app/admin/server/controllers/index.server.controller.js
+++ app/admin/server/controllers/index.server.controller.js
@@ -38,7 +38,7 @@ exports.dashboard = function(req, res) {
   console.log("ID:38 :: index.server.controller :: dashboard() "); 
   if (!req.isAuthenticated()) return res.redirect('/admin/signin'); 
   
-  Avaliacao.find({},function(err, avaliacoes) {
+  Avaliacao.find({}, function(err, avaliacoes) {
       if (err) {
          return next(err);
       } else {
@@ -47,45 +47,59 @@ exports.dashboard = function(req, res) {
   });
 
   Avaliacao.aggregate([ 
-        { 
-          $group: { 
-            _id: '$nota_conteudo', 
-            count: {$sum: 1} 
-          } 
-        } 
-  ], function (err, result) { 
+          { 
+           // $sort: {nota_conteudo: 1},
+            $group: { 
+              _id: '$nota_conteudo', 
+              count: {$sum: 1} 
+            } 
+          },
+          {
+              $sort: {
+                  _id: 1
+              }
+          },
+    ], function (err, result) { 
 
        global.register["NotaConteudoResult"] = result;
 
+        Avaliacao.aggregate([ 
+              { 
+                $group: { 
+                  _id: '$conservacao', 
+                  count: {$sum: 1} 
+                } 
+              } ,
+              {
+                  $sort: {
+                      _id: 1
+                  }
+              },
+
+        ], function (err, result) { 
+
+            var totalConservacao = 0;
+            for (var i = result.length - 1; i >= 0; i--) { 
+              result[i]["label"] = getConservacaoCase(result[i]._id); 
+            }; 
+
+              res.status(res.statusCode) 
+                .render('Admin/dashboard',{ 
+                   pathTheme: 'AdminLTE', 
+                   title: 'Administração :: Dashboard', 
+                   aMessage: [], //"Trabalho em progresso!"
+                   totalConservacao: totalConservacao, 
+                   NotaConteudoResult: global.register["NotaConteudoResult"],
+                   totalAvaliacao: global.register["avaliacoes"].length,
+                   dataDashbaord: result, 
+                   user: req.user // JSON.stringify(req.user)
+                });
+
+        }); 
+
   }); 
 
-  Avaliacao.aggregate([ 
-        { 
-          $group: { 
-            _id: '$conservacao', 
-            count: {$sum: 1} 
-          } 
-        } 
-  ], function (err, result) { 
-
-      var totalConservacao = 0;
-      for (var i = result.length - 1; i >= 0; i--) { 
-        result[i]["label"] = getConservacaoCase(result[i]._id); 
-      }; 
-
-        res.status(res.statusCode) 
-          .render('Admin/dashboard',{ 
-             pathTheme: 'AdminLTE', 
-             title: 'Administração :: Dashboard', 
-             aMessage: [], //"Trabalho em progresso!"
-             totalConservacao: totalConservacao, 
-             NotaConteudoResult: global.register["NotaConteudoResult"],
-             totalAvaliacao: global.register["avaliacoes"].length,
-             dataDashbaord: result, 
-             user: req.user // JSON.stringify(req.user)
-          });
-
-  }); 
+ 
 
 
 
